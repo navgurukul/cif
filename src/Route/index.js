9fb8e42c94b6aa1aadc8312c4aceccc99fe6b329
pratikartik <kartikshikhare26@gmail.com>
2023-01-24 12:37:05 +0530

    added scroll to top

--- src/Route/index.js
+++ src/Route/index.js
@@ -6,18 +6,21 @@ import Home from "../Page/Home";
 import Partner from "../Page/Partner";
 import Faq from "../Page/Faq/Faq";
 import JoinUs from "../Page/JoinUs/JoinUs";
+import SmoothScroll from "./SmoothScroll";
 
 function Routing() {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/partner" element={<Partner />} />
-          <Route exact path="/faqs" element={<Faq />} />
-          <Route exact path="/join" element={<JoinUs />} />
-        </Routes>
+        <SmoothScroll>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/partner" element={<Partner />} />
+            <Route exact path="/faqs" element={<Faq />} />
+            <Route exact path="/join" element={<JoinUs />} />
+          </Routes>
+        </SmoothScroll>
         <Footer />
       </BrowserRouter>
     </>
