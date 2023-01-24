c55fda534f3db62b6dcb6c19b136b16966f59b00
pratikartik <kartikshikhare26@gmail.com>
2023-01-24 12:14:21 +0530

    fixed some bug

--- src/Page/JoinUs/components/WhatWeNeed.js
+++ src/Page/JoinUs/components/WhatWeNeed.js
@@ -25,21 +25,21 @@ function WhatWeNeed() {
           marginTop={6}
         >
           <Grid item>
-            <Card elevation={4} className={classes.NeedCardType1}>
+            <Card elevation={2} className={classes.NeedCardType1}>
               Have a tech degree (Computer Science, Information Technology,
               etc.); also apply if you aren’t a tech graduate but are good at
               tech
             </Card>
           </Grid>
           <Grid item>
-            <Card elevation={4} className={classes.NeedCardType1}>
+            <Card elevation={2} className={classes.NeedCardType1}>
               Believe in the Code India Fellowship's mission of using education
               to provide young women an opportunity to attain agency & financial
               independence
             </Card>
           </Grid>
           <Grid item>
-            <Card elevation={4} className={classes.NeedCardType1}>
+            <Card elevation={2} className={classes.NeedCardType1}>
               Find the idea of utilizing a year facilitating learning for young
               women and girls in a low-income rural school while becoming a
               better version of yourselves exciting
@@ -54,19 +54,19 @@ function WhatWeNeed() {
           className={classes.NeedCardInRow2}
         >
           <Grid item>
-            <Card elevation={4} className={classes.NeedCardType2}>
+            <Card elevation={2} className={classes.NeedCardType2}>
               Have an aptitude for critical thinking and problem solving
             </Card>
           </Grid>
           <Grid item>
-            <Card elevation={4} className={classes.NeedCardType2}>
+            <Card elevation={2} className={classes.NeedCardType2}>
               Are fluent in English and have a working knowledge of Hindi
             </Card>
           </Grid>
         </Grid>
       </Container>
       <Container
-        maxWidth="xl"
+        maxWidth="lg"
         style={{
           margin: "4rem 0",
           border: "1px solid grey",
--- src/Page/Partner/index.js
+++ src/Page/Partner/index.js
@@ -28,7 +28,8 @@ function Partner() {
           justifyContent: "center",
           mt: "81px",
           mb: "64px",
-        }}>
+        }}
+      >
         Created by Industry Leaders
       </Typography>
       <Grid container spacing={2}>
@@ -37,14 +38,16 @@ function Partner() {
             sx={{
               display: "flex",
               justifyContent: "center",
-            }}>
+            }}
+          >
             <Card
               // className={isActive ? classes.mobileCard : classes.desktopCard}
               sx={{
                 border: "1px dashed #7743DB",
                 borderRadius: "8px",
                 background: "#FFFFFF",
-              }}>
+              }}
+            >
               <CardContent>
                 <img
                   src={require("../../assets/niti-aayog.svg").default}
@@ -80,14 +83,16 @@ function Partner() {
               display: "flex",
               justifyContent: "center",
               marginTop: "17px",
-            }}>
+            }}
+          >
             <Card
               sx={{
                 border: "1px dashed #7743DB",
                 borderRadius: "8px",
                 background: "#FFFFFF",
                 padding: !isActive && "32px",
-              }}>
+              }}
+            >
               <CardContent>
                 <img
                   src={require("../../assets/navgurukul.svg").default}
@@ -96,24 +101,35 @@ function Partner() {
                   className={classes.image}
                 />
                 <Typography variant="subtitle1" sx={{ mb: "16px" }}>
-                  NITI AAYOG
+                  NAVGURUKUL
                 </Typography>
                 <Typography variant="body1" sx={{ mb: "16px" }}>
-                  NITI Aayog is the Government of India’s apex public policy
-                  think tank. It also functions as a state-of-the-art resource
-                  center to promote research and innovation and provide
-                  strategic policy vision for the government. NITI Aayog designs
-                  strategic and long-term policies, programs, and initiatives to
-                  support the states.
+                  NavGurukul is a not-for-profit founded in 2016 and is on a
+                  mission to create access to aspirational careers for students
+                  from marginalized communities.
                 </Typography>
                 <Typography variant="body1" sx={{ mb: "16px" }}>
-                  NITI Aayog’s Education Vertical strives to facilitate school
-                  readiness, socio-emotional learning, critical and higher-order
-                  thinking, and grade-level competency among children. It seeks
-                  to empower youth with employability skills, research
-                  temperament, and subject matter expertise through a
-                  high-quality, accessible, equitable, accountable, and
-                  affordable education system.
+                  NavGurukul’s flagship Residential Program works with learners
+                  from underserved communities to help them become programmers
+                  and gives them a guaranteed job once they finish the program.
+                  Currently, NavGurukul is training 1000+ students in seven
+                  fully funded residential centers across various states in
+                  India. Six of the seven campuses have only girl students. The
+                  focus on gender is a conscious choice, as inclusion is one of
+                  the core principles guiding NavGurukul’s choices and focus
+                  areas. The residential program is open for students who have
+                  finished just 10th grade and are 17 years of age. NavGurukul
+                  has placed around 500+ students in software jobs in the tech
+                  industry with various IT giants and product startups.
+                </Typography>
+                <Typography variant="body1" sx={{ mb: "16px" }}>
+                  Through Meraki, NavGurukul works with students (and teachers)
+                  from government schools and ITIs through an online offering to
+                  help students learn basic English, touch typing, and
+                  programming. Code India Fellowship is a high-impact ambitious
+                  program to onboard high-aptitude and mission-aligned young
+                  graduates for a one-year fellowship and place them in
+                  government high schools.
                 </Typography>
               </CardContent>
             </Card>
@@ -124,14 +140,16 @@ function Partner() {
             sx={{
               display: "flex",
               justifyContent: "center",
-            }}>
+            }}
+          >
             <Card
               sx={{
                 border: "1px dashed #7743DB",
                 borderRadius: "8px",
                 background: "#FFFFFF",
                 padding: !isActive && "32px",
-              }}>
+              }}
+            >
               <CardContent>
                 <img
                   src={require("../../assets/amazon.svg").default}
@@ -140,33 +158,33 @@ function Partner() {
                   className={classes.image}
                 />
                 <Typography variant="subtitle1" sx={{ mb: "16px" }}>
-                  NITI AAYOG
+                  AMAZON
                 </Typography>
                 <Typography variant="body1" sx={{ mb: "16px" }}>
-                  NITI Aayog is the Government of India’s apex public policy
-                  think tank. It also functions as a state-of-the-art resource
-                  center to promote research and innovation and provide
-                  strategic policy vision for the government. NITI Aayog designs
-                  strategic and long-term policies, programs, and initiatives to
-                  support the states.
+                  Amazon is one of the most recognizable brands in the world and
+                  is a technology company with a presence across e-commerce,
+                  cloud computing, online advertising, digital streaming, and
+                  artificial intelligence. In India Amazon started operations in
+                  2013 and now serves 150 million Indian customers.
                 </Typography>
                 <Typography variant="body1" sx={{ mb: "16px" }}>
-                  NITI Aayog’s Education Vertical strives to facilitate school
-                  readiness, socio-emotional learning, critical and higher-order
-                  thinking, and grade-level competency among children. It seeks
-                  to empower youth with employability skills, research
-                  temperament, and subject matter expertise through a
-                  high-quality, accessible, equitable, accountable, and
-                  affordable education system.
+                  Through Amazon Future Engineer (AFE), a comprehensive
+                  childhood-to-career program aimed at increasing access to
+                  computer science education for children and young adults who
+                  typically lack these opportunities, Amazon supports those who
+                  face disproportionate barriers to education, including
+                  household income and gender, to develop and learn computer
+                  science skills to have equal opportunities in the future world
+                  of work.
                 </Typography>
                 <Typography variant="body1" sx={{ mb: "16px" }}>
-                  NITI Aayog’s Education Vertical strives to facilitate school
-                  readiness, socio-emotional learning, critical and higher-order
-                  thinking, and grade-level competency among children. It seeks
-                  to empower youth with employability skills, research
-                  temperament, and subject matter expertise through a
-                  high-quality, accessible, equitable, accountable, and
-                  affordable education system.
+                  Amazon supports underprivileged and under-served students
+                  learn computer science in five countries – the US, the UK,
+                  France, Canada, and India. Amazon and its partners support
+                  more than 900 government schools across seven states in India.
+                  Amazon also supports girls from low-income backgrounds looking
+                  to pursue careers in technology with scholarships, mentorship,
+                  and internships.
                 </Typography>
               </CardContent>
             </Card>
